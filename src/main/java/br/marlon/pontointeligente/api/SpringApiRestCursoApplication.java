c395e2d7df76a8fce1bded2eff39025e11ee70ec
marlon123 <marlonlopes.p@gmail.com>
2018-05-13 01:56:13 -0300

    setup dos arquivos

---
+++ src/main/java/br/marlon/pontointeligente/api/SpringApiRestCursoApplication.java
@@ -0,0 +1,12 @@
+package br.marlon.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiRestCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiRestCursoApplication.class, args);
+	}
+}
